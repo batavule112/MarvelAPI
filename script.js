7aca108548ccdb3a6e63650ec484dd70f8f6e11d
batavule112 <batavule112@gmail.com>
2018-02-21 17:39:59 +0100

    sve async

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 function prikaziStranu(id) {
 	var maxStrana = 20;
 	for ( i = 1; i < maxStrana; i++ ) {
-		if(document.getElementById("strana"+i))
-			document.getElementById("strana"+i).style.display='none';
-		}
-		if (document.getElementById("strana"+id)) 
-			document.getElementById("strana"+id).style.display='block';	
+		if($("#strana"+i))
+			$("#strana"+i).css('display', 'none');
+	}
+	if ($("#strana"+id))
+		$("#strana"+id).css('display', 'block');
 }
 // plugin da kasni search
 var zakasnjenje = null;  
@@ -23,80 +23,61 @@ function trazi() {
 	var pretraga = new XMLHttpRequest();
 	var pretrazivac = document.getElementById("text").value;
 	var url = "http://gateway.marvel.com/v1/public/characters?&ts=1&apikey=3912be687c89f571e89351c2ffb553d2&hash=3a08d56d5470a38378ad1df45e07b065";
-	pretraga.open('GET', url + "&nameStartsWith=" + pretrazivac + "&limit=12" + "&offset=" + offsetVrednost[0], true);
-	pretraga.onreadystatechange = function(){
-		if(this.readyState == 2 && this.status === 200) {
-		document.getElementById("poruka").innerHTML = "Ucitavam...";
-		}
-	}
-	pretraga.onreadystatechange = function(){
-		if(this.readyState == 3 && this.status === 200) {
-		document.getElementById("poruka").innerHTML = "Uspesno ucitavanje!";
-		}
-	}
-	pretraga.onload = function(){
-		if(this.status === 200){
-			var rezultati = JSON.parse(this.responseText);
-			document.getElementById("footer").innerHTML = rezultati.attributionHTML;
-			document.getElementById("info").innerHTML = "Pronadjeni su " + rezultati.data.total + " rezultata. ";
-			 var brojstrana = rezultati.data.total / 12;
+	function info (){
+	return $.get(url+"&nameStartsWith="+pretrazivac+"&limit=12"+"&offset="+offsetVrednost[0]);}
+	info().done(function(data){
+			document.getElementById("footer").innerHTML = data.attributionHTML;
+			document.getElementById("info").innerHTML = "Pronadjeni su " + data.data.total + " rezultata. ";
+			 var brojstrana = data.data.total / 12;
 			 if ( brojstrana > 0 ) {
 				for ( g = 0; g < brojstrana; g++ ) {
-					pretraga.open('GET', url + "&nameStartsWith=" + pretrazivac + "&limit=12" + "&offset=" + offsetVrednost[g], false);
-					pretraga.onload = function(){
-					if(this.status === 200){
-						var rezultati1 = JSON.parse(this.responseText);
-						console.log(rezultati1.data.total);
-						var strana = "strana" + [g+1];
-						var string = "";
-						string += "<div class='nema' id='" + strana + "'>";
-						string += "<div class='row'>";
-						for(var i = 0; i<rezultati1.data.results.length; i++) {
-							var element = rezultati1.data.results[i];
-						string += "<div class='col-md-3 col-sm-6 kartica'>";
-						string += "<div class='card'>";
-						string += "<a href='"+element.urls[1].url+"'target='_blank'>";
-						string += "<img src='"+element.thumbnail.path +"/portrait_fantastic."+element.thumbnail.extension+"'/>";
-						string += "</a>";
-						string += "<h5>" + element.name + "</h5>";
-						string += "</div>";
-						string += "</div>";
-						if (( i+1 ) % 4 == 0) {
-						string += "</div>";
-						string += "<div class='row'>" 
-							}
+					// dodavanje nav i strana
+					$('#bla').append("<li class='page-item'><a class='page-link' href='javascript: onClick=prikaziStranu(\""+ [g+1] +"\");'>"+ [g+1] +"</a></li>");
+						$('.page-item:first-child').addClass('active');
+						if ( brojstrana <= 1  ) {
+							$('.page-item:first-child').css('display', 'none');
 						}
-						string += "<div>";
-						// dodavanje nav i strana
-						document.getElementById('bla').insertAdjacentHTML( 'beforeend', "<li class='page-item'><a class='page-link' href='javascript: onClick=prikaziStranu(\""+ [g+1] +"\");'>"+ [g+1] +"</a></li>");
-						document.getElementsByClassName('page-item')[0].classList.add("active");
-						document.getElementById('strane').insertAdjacentHTML( 'afterend', string);
-						document.getElementById('strana1').style.display = 'block';
-						if ( brojstrana < 1  ) {
-							document.getElementsByClassName('page-item')[0].style.display = 'none';
-										}
-									}
+						try{throw g}
+						catch(g) {
+						$.ajax({
+							url: url + "&nameStartsWith=" + pretrazivac + "&limit=12" + "&offset=" + offsetVrednost[g],
+							type: "GET",
+							success: function(rezultati1) {
+							var strana = "strana" + [g+1];
+							var string = "";
+							string += "<div class='nema' id='" + strana + "'>";
+							string += "<div class='row'>";
+							for(var i = 0; i<rezultati1.data.results.length; i++) {
+								var element = rezultati1.data.results[i];
+							string += "<div class='col-md-3 col-sm-6 kartica'>";
+							string += "<div class='card'>";
+							string += "<a href='"+element.urls[1].url+"'target='_blank'>";
+							string += "<img src='"+element.thumbnail.path +"/portrait_fantastic."+element.thumbnail.extension+"'/>";
+							string += "</a>";
+							string += "<h5>" + element.name + "</h5>";
+							string += "</div>";
+							string += "</div>";
+							if (( i+1 ) % 4 == 0) {
+							string += "</div>";
+							string += "<div class='row'>" 
 								}
-							pretraga.send();
 							}
-						}
-					}
-					// selektovanje activne strane
-					var oznaci = document.getElementsByClassName("page-item");
-					for(var i = 0; i < oznaci.length; i++) {
-        				oznaci[i].onclick = function(){
-        				var ovo = oznaci[0];
-        				while(ovo) {
-            			if(ovo.tagName === "LI"){
-                			ovo.classList.remove("active");
-                			}
-                		ovo = ovo.nextSibling;
-            		}
-        		this.classList.add("active");  
-    		};
-		}
+							string += "<div>";
+							$('.right').append(string);
+							$('#strana1').css('display', 'block');
+										}
+									});
+    							}
+							}
+						}			
+$(".page-item").click(function() {
+	if ($('.page-item').hasClass("active")) {
+		$('.page-item').removeClass("active");
+		$(this).addClass("active");
 	}
-	pretraga.send();
+	else $(this).addClass("active");
+		});
+	});
 }
 // jumbotron slidein
 function pali() {
